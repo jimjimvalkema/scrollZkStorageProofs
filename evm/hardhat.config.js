6d6c82f234d5d9315cad638361231fdd7f146eec
jjvalkema <jjvalkema@protonmail.com>
2024-06-30 20:21:12 +0200

    retried with hardhat but in different folder since hardhat messes with my package.json :/

---
+++ evm/hardhat.config.js
@@ -0,0 +1,34 @@
+// type: "module" not supported in package.js which is cringe
+require("@nomicfoundation/hardhat-toolbox");
+const { vars } = require("hardhat/config");
+
+const SEPOLIA_SCROLL_ETHERSCAN_KEY = vars.get("SEPOLIA_SCROLL_ETHERSCAN_KEY");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.23",
+  // https://docs.scroll.io/en/developers/developer-quickstart/#hardhat
+  networks: {
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+        [PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      scrollSepolia:SEPOLIA_SCROLL_ETHERSCAN_KEY,
+    },
+    customChains: [
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia.scrollscan.com/',
+        },
+      },
+    ],
+  },
+};
