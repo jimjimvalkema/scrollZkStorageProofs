77d29b98fa19a12ec6c2df1671647fa47e6f64de
jimjimvalkema <jjvalkema@protonmail.com>
2024-08-05 15:48:12 +0200

    remove tutorial files and did imports

--- zkwormholesExample/website/main.js
+++ zkwormholesExample/website/main.js
@@ -1,37 +1,66 @@
-import circuit from './circuit/target/circuit.json';
+import { ethers } from 'ethers';
+window.ethers = ethers
+
+import circuit from '../circuits/smolProver/target/zkwormholesEIP7503.json';
 import { BarretenbergBackend, BarretenbergVerifier as Verifier } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
 
-function display(container, msg) {
-  const c = document.getElementById(container);
-  const p = document.createElement('p');
-  p.textContent = msg;
-  c.appendChild(p);
+
+import {abi as  contractAbi} from '../artifacts/contracts/Token.sol/Token.json'
+import {getSafeRandomNumber} from '../scripts/getProofInputs'
+const CONTRACT_ADDRESS = "0x0C506a4855Ab156DB9A504AE5C2c9a50f2FF9BA2"
+const FIELD_LIMIT = 21888242871839275222246405745257275088548364400416034343698204186575808495617n //using poseidon so we work with 254 bits instead of 256
+const SEPOLIA = {
+    chainId: "0xaa36a7",
+    rpcUrls: ["https://rpc.sepolia.org/"],
+    chainName: "sepolia",
+    nativeCurrency: {
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18
+    },
+    blockExplorerUrls: ["https://sepolia.etherscan.io/"]
 }
 
-document.getElementById('submitGuess').addEventListener('click', async () => {
-  const x = parseInt(document.getElementById('guessInput').value);
-  const input = { x, y: 2 };
+const contract = await getContractWithSigner()
 
-  // try {
-  const backend = new BarretenbergBackend(circuit);
-  const noir = new Noir(circuit);
-  //await setup(); // let's squeeze our wasm inits here
+async function getContractWithSigner({abi=contractAbi, chain=SEPOLIA, contractAddress=CONTRACT_ADDRESS}={}) {
+  return await dumpErrorsInUi(
+    async ()=>{
+      const provider = new ethers.BrowserProvider(window.ethereum)
+      await switchNetwork(chain, provider)
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer)
+      return contract
+    }
+  )
 
-  display('logs', 'Generating proof... ⌛');
-  const { witness } = await noir.execute(input);
-  const proof = await backend.generateProof(witness);
-  display('logs', 'Generating proof... ✅');
-  display('results', proof.proof);
-  display('logs', 'Verifying proof... ⌛');
-  const isValid = await backend.verifyProof(proof);
-  if (isValid) { 
-    display('logs', 'Verifying proof... ✅') 
-  } else {
-    display('logs', 'Oh 💔 Wrong guess');
-  }
-  // here's where love happens
+}
 
+async function switchNetwork(network,provider) {
+  try {
+      await provider.send("wallet_switchEthereumChain",[{ chainId: network.chainId }]);
 
+    } catch (switchError) {
+      window.switchError = switchError
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (switchError.error && switchError.error.code === 4902) {
+        try {
+          await provider.send("wallet_addEthereumChain",[network]);
 
-});
+        } catch (addError) {
+          // handle "add" error
+        }
+      }
+      // handle other "switch" errors
+    }
+}
+
+async function dumpErrorsInUi(func, args=[]) {
+  try {
+      await func(...args)
+  } catch (error) {
+      console.error(error)
+      document.querySelector("#errors").innerText += `${func.name}:${error}`    
+  } 
+}
