1bb690b5a71f90b01cc82a347686c115fb6731b8
jimjimvalkema <jjvalkema@protonmail.com>
2024-08-10 22:28:03 +0200

    moved zkwormholesExample to its own repo and removed the poseidon hash submodule

--- zkwormholesExample/ignition/modules/Token.cjs
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("TokenModule", (m) => {
-  const token = m.contract("Token", [], {
-    value: 0n,
-  });
-  return { token };
-});
